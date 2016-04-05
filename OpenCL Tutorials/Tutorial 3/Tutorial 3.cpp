aa55ab8ffca79e7076a78a5ab9f7b9bb633fd0a3
- <->
2016-04-05 14:02:43 +0100

    Histogram Function Implemented and Code Commented

--- OpenCL Tutorials/Tutorial 3/Tutorial 3.cpp
+++ OpenCL Tutorials/Tutorial 3/Tutorial 3.cpp
@@ -74,8 +74,9 @@ int main(int argc, char **argv) {
 		std::vector<myType> entryTime;*/
 		std::vector<intType> entryTemp;
 
-		string filePath = "C:\\Users\\Computing\\Documents\\GitHub\\Parallel\\OpenCL Tutorials\\x64\\Debug\\Files\\temp_lincolnshire_short.txt";
-		int entryLength = 0;
+		//uses file path to open the .txt data file
+		string filePath = "C:\\Users\\Computing\\Documents\\GitHub\\Parallel\\OpenCL Tutorials\\x64\\Debug\\Files\\temp_lincolnshire.txt";
+		int entryLength = 0; //variable to track the length of the data retroactively
 		try {
 			ifstream fileLoc(filePath);
 			string fileLine;
@@ -85,11 +86,11 @@ int main(int argc, char **argv) {
 				entryMonth.push_back(mm);
 				entryDay.push_back(dd);
 				entryTime.push_back(tm);*/
-				entryTemp.push_back((stof(tp)) * 10);
+				entryTemp.push_back((stof(tp)) * 10); //reads the 6th element on each row of data as the temperature
 
 				entryLength++;
 				if (entryLength % 1000 == 0) {
-					std::cout << "\rLine Count:" << (entryLength) << flush;
+					std::cout << "\rLine Count:" << (entryLength) << flush; //shows the user how many lines have currently been processed
 				}
 			}
 			std::cout << "\rLine Count:" << (entryLength) << flush;
@@ -99,8 +100,8 @@ int main(int argc, char **argv) {
 			std::cout << "File input error";
 		}
 
-		size_t entryElements = entryTemp.size();
-		size_t entryInputSize = entryTemp.size()*sizeof(intType);//size in bytes, used for buffer
+		size_t entryElements = entryTemp.size(); //amount of elements in the temperature array
+		size_t entryInputSize = entryTemp.size()*sizeof(intType); //size in bytes, used for buffer
 
 		size_t entryGroupSize;
 		size_t entryGroups;
@@ -110,12 +111,13 @@ int main(int argc, char **argv) {
 		{
 			if (entryLength % i == 0) {
 				entryGroupSize = i;
-				entryGroups = entryLength / entryGroupSize; //uses the amount of threads to determint he amount of work groups
+				entryGroups = entryLength / entryGroupSize; //uses the amount of threads to determin the amount of work groups
 				break;
 			}
 		}
 
 		//creates the output vectors with a size 1 higher than there are work groups
+		//the 0th element will be left empty for future use
 		std::vector<intType> outTempMin(entryGroups + 1);
 		std::vector<intType> outTempMax(entryGroups + 1);
 		std::vector<floatType> outTempAvg(entryGroups + 1);
@@ -127,19 +129,22 @@ int main(int argc, char **argv) {
 		cl::Buffer bufferOutTempMax(context, CL_MEM_READ_WRITE, entryOutputSize);
 		cl::Buffer bufferOutTempAvg(context, CL_MEM_READ_WRITE, entryOutputSize);
 
+		//writes and fills buffers
 		queue.enqueueWriteBuffer(bufferInTemp, CL_TRUE, 0, entryInputSize, &entryTemp[0]);
 		queue.enqueueFillBuffer(bufferOutTempMin, 0, 0, entryOutputSize);
 		queue.enqueueFillBuffer(bufferOutTempMax, 0, 0, entryOutputSize);
 		queue.enqueueFillBuffer(bufferOutTempAvg, 0, 0, entryOutputSize);
 
+		//kernel for finding the minumum value
 		cl::Kernel kernel_1 = cl::Kernel(program, "value_min");
 		kernel_1.setArg(0, bufferInTemp);
 		kernel_1.setArg(1, bufferOutTempMin);
 		kernel_1.setArg(2, cl::Local(entryGroupSize*sizeof(myType)));
 
-		queue.enqueueNDRangeKernel(kernel_1, cl::NullRange, cl::NDRange(entryElements), cl::NDRange(entryGroupSize));//call all kernels in a sequence
-		queue.enqueueReadBuffer(bufferOutTempMin, CL_TRUE, 0, entryOutputSize, &outTempMin[0]);//Copy the results from device to host
+		queue.enqueueNDRangeKernel(kernel_1, cl::NullRange, cl::NDRange(entryElements), cl::NDRange(entryGroupSize)); //call all kernels in a sequence
+		queue.enqueueReadBuffer(bufferOutTempMin, CL_TRUE, 0, entryOutputSize, &outTempMin[0]); //Copy the results from device to host
 
+		//kernel for finding the maximum value
 		cl::Kernel kernel_2 = cl::Kernel(program, "value_max");
 		kernel_2.setArg(0, bufferInTemp);
 		kernel_2.setArg(1, bufferOutTempMax);
@@ -148,6 +153,7 @@ int main(int argc, char **argv) {
 		queue.enqueueNDRangeKernel(kernel_2, cl::NullRange, cl::NDRange(entryElements), cl::NDRange(entryGroupSize));
 		queue.enqueueReadBuffer(bufferOutTempMax, CL_TRUE, 0, entryOutputSize, &outTempMax[0]);
 
+		//kernel for summing all values, used later to find the average
 		cl::Kernel kernel_3 = cl::Kernel(program, "value_avg");
 		kernel_3.setArg(0, bufferInTemp);
 		kernel_3.setArg(1, bufferOutTempAvg);
@@ -156,79 +162,91 @@ int main(int argc, char **argv) {
 		queue.enqueueNDRangeKernel(kernel_3, cl::NullRange, cl::NDRange(entryElements), cl::NDRange(entryGroupSize));
 		queue.enqueueReadBuffer(bufferOutTempAvg, CL_TRUE, 0, entryOutputSize, &outTempAvg[0]);
 
-		outTempMin[0] = 99999;
-		outTempMax[0] = -99999;
-		for (int i = 1; i <= entryGroups; i++) {
-			if (outTempMin[0] > outTempMin[i])
-				outTempMin[0] = outTempMin[i];
+		//sets the 0th element of the output vectors to the same as the 1st element
+		outTempMin[0] = outTempMin[1];
+		outTempMax[0] = outTempMax[1];
+		outTempAvg[0] = outTempAvg[1];
 
-			if (outTempMax[0] < outTempMax[i])
-				outTempMax[0] = outTempMax[i];
+		//starts from the 2nd element of each vector
+		for (int i = 2; i <= entryGroups; i++) {
+			//compares the 0th element of the minimum vector to each other element 
+			if (outTempMin[0] > outTempMin[i]) //if the 0th element is larger than the current element
+				outTempMin[0] = outTempMin[i]; //sets the 0th element to the same value as the current element
 
-			outTempAvg[0] += outTempAvg[i];
+			//compares the 0th element of the maximum vector to each other element 
+			if (outTempMax[0] < outTempMax[i]) //if the 0th element is lower than the current element
+				outTempMax[0] = outTempMax[i]; //sets the 0th element to the same value as the current element
+
+			outTempAvg[0] += outTempAvg[i]; //adds the current element to the 0th element to create the summed total
 		}
 
-		outTempAvg[0] /= entryElements;
+		outTempAvg[0] /= entryElements; //divides the summed total of all the elements by the amount of elements to find the average
 
+		//outputs the minimum, maximum and average values to the console
 		std::cout << std::endl;
 		std::cout << "Min = " << (float)outTempMin[0] / 10 << std::endl;
 		std::cout << "Max = " << (float)outTempMax[0] / 10 << std::endl;
 		std::cout << "Avg = " << outTempAvg[0] / 10 << std::endl;
+		std::cout << endl;
 
-		/*typedef int mytype;
+		int histRange = (ceil((float)outTempMax[0] / 10)) - (floor((float)outTempMin[0] / 10)) + 1; //finds the range of values using the minimum and maximum values
+		int histOffset = floor((float)outTempMin[0] / 10); //uses the minimum value as an offset so that all values can be above 0
+		if (histOffset > 0) //checks to see if the offset is above 0
+			histOffset = 0;	//sets the offset to 0, as it is unnecessary in this scenario
 
-		//Part 4 - memory allocation
-		//host - input
-		std::vector<mytype> A(10, 1);//allocate 10 elements with an initial value 1 - their sum is 10 so it should be easy to check the results!
+		std::vector<intType> hist(histRange); //creates a new vector for the histogram
+		size_t histSize = hist.size()*sizeof(intType); //size in bytes
 
-		//the following part adjusts the length of the input vector so it can be run for a specific workgroup size
-		//if the total input length is divisible by the workgroup size
-		//this makes the code more efficient
-		size_t local_size = 10;
+		//buffer for histogram
+		cl::Buffer bufferHist(context, CL_MEM_READ_WRITE, histSize);
+		queue.enqueueFillBuffer(bufferHist, 0, 0, histSize);
 
-		size_t padding_size = A.size() % local_size;
+		//kernel for histogram
+		cl::Kernel kernel_4 = cl::Kernel(program, "value_hist");
+		kernel_4.setArg(0, bufferInTemp);
+		kernel_4.setArg(1, bufferHist);
+		kernel_4.setArg(2, histRange);
+		kernel_4.setArg(3, histOffset);
 
-		//if the input vector is not a multiple of the local_size
-		//insert additional neutral elements (0 for addition) so that the total will not be affected
-		if (padding_size) {
-		//create an extra vector with neutral values
-		std::vector<int> A_ext(local_size-padding_size, 0);
-		//append that extra vector to our input
-		A.insert(A.end(), A_ext.begin(), A_ext.end());
+		queue.enqueueNDRangeKernel(kernel_4, cl::NullRange, cl::NDRange(entryElements), cl::NDRange(entryGroupSize));
+		queue.enqueueReadBuffer(bufferHist, CL_TRUE, 0, histSize, &hist[0]);
+
+		int histStart = 0;
+		int histEnd = histRange;
+
+		//finds the lowest percentile for the histogram
+		int histCount = 0;
+		for (int i = 0; i < histRange; i++) {
+			histCount += hist[i]; //sums all values that represent the first ~1% of data
+			if (histCount > (entryElements / 100)) {
+				hist[i] = histCount; //adds all summed values into the lowest relevent bin
+				histStart = i; //records which bin the lowest relevent bin
+				break;
+			}
 		}
 
-		size_t input_elements = A.size();//number of input elements
-		size_t input_size = A.size()*sizeof(mytype);//size in bytes
-		size_t nr_groups = input_elements / local_size;
+		//finds the highest percentile for the histogram
+		histCount = 0;
+		for (int i = histRange - 1; i >= 0; i--) {
+			histCount += hist[i]; //sums all values that represent the last ~1% of data
+			if (histCount > (entryElements / 100)) {
+				hist[i] = histCount; //adds all summed values into the highest relevent bin
+				histEnd = i; //records which bin the highest relevent bin
+				break;
+			}
+		}
 
-		//host - output
-		std::vector<mytype> B(input_elements);
-		size_t output_size = B.size()*sizeof(mytype);//size in bytes
-
-		//device - buffers
-		cl::Buffer buffer_A(context, CL_MEM_READ_ONLY, input_size);
-		cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, output_size);
-
-		//Part 5 - device operations
-
-		//5.1 copy array A to and initialise other arrays on device memory
-		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, input_size, &A[0]);
-		queue.enqueueFillBuffer(buffer_B, 0, 0, output_size);//zero B buffer on device memory
-
-		//5.2 Setup and execute all kernels (i.e. device code)
-		cl::Kernel kernel_1 = cl::Kernel(program, "reduce_add_1");
-		kernel_1.setArg(0, buffer_A);
-		kernel_1.setArg(1, buffer_B);
-		//		kernel_1.setArg(2, cl::Local(local_size*sizeof(mytype)));//local memory size
-
-		//call all kernels in a sequence
-		queue.enqueueNDRangeKernel(kernel_1, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size));
-
-		//5.3 Copy the result from device to host
-		queue.enqueueReadBuffer(buffer_B, CL_TRUE, 0, output_size, &B[0]);
-
-		std::cout << "A = " << A << std::endl;
-		std::cout << "B = " << B << std::endl;*/
+		//outputs the histogram
+		std::cout << "Temperature Histogram" << endl;
+		for (int i = histStart; i <= histEnd; i++) { //uses the lowest and highest relevant bins to condense all outlying data into the first and last bins
+			if (i == histStart)
+				std::cout << "<="; //adds the "<=" symbol to the first bin of the histogram to show that it also represents outlying values 
+			std::cout << (i + histOffset);
+			if (i == histEnd)
+				std::cout << "+"; //adds the "+" symbol to the last bin of the histogram to show that it also represents outlying values
+			std::cout << ": ";
+			std::cout << hist[i] << endl;
+		}
 	}
 	catch (cl::Error err) {
 		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
